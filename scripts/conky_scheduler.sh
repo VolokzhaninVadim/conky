3dcb8e7253cf014e44af7181ff7fc4da9ab8a1f9
volokzhanin <volokzhanin@yandex.ru>
2021-12-25 14:52:58 +1000

    raspberry

--- scripts/conky_scheduler.sh
+++ scripts/conky_scheduler.sh
@@ -1,3 +1,3 @@
 #!/bin/sh 
-conky -p 15 -c /mnt/0/documents/projects/pc_settings/conky/scripts/.conkyrc_system -a top_right -x 10 -y 0 -d 
-conky -p 10 -c /mnt/0/documents/projects/pc_settings/conky/scripts/.conkyrc_add_info -a top_left -x 70 -y 0 -d
+conky -p 15 -c /home/volokzhanin/conky/scripts/.conkyrc_system -a top_right -x 10 -y 0 -d 
+conky -p 10 -c /home/volokzhanin/conky/scripts/.conkyrc_add_info -a top_left -x 70 -y 0 -d
